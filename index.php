64d3b6edc5a47d108c44a68281ba389b3d7506ba
krprince421 <61113784+krprince421@users.noreply.github.com>
2022-04-10 19:28:56 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>Html Quiz</title>
+    <link rel="stylesheet" href="styles.css" > 
+</head>
+<body>
+
+    <header class = " header text-center">
+        <ul>
+            <a href="index.php"><li>Home</li></a>
+            <a href="htmlpage.php"><li>HTML</li></a>
+            <a href="#"><li>CSS</li></a>
+            <a href="#"><li>PHP</li></a>
+        </ul>
+
+    </header>
+    
+    <body class = "main-content">
+    
+        <div class = "flex-container">
+            
+            <div class = "html-flex text-center">
+                    <h1>HTML</h1>
+                    <img style = "height:100px ; width:100px;" src = "html.png">
+            </div>
+            
+            <div class = "css-flex text-center">
+                    <h1>CSS</h1>
+                    <img  style = "height:100px ; width:100px;" src = "css.png">
+            </div>
+            <div class = "js-flex text-center">
+                    <h1>JAVASCRIPT</h1>
+                    <img style = "height:100px ; width:100px;" src = "js.jpg">
+            </div>
+            <div class = "php-flex text-center">
+            
+                    <h1>PHP</h1>
+                    <img style = "height:100px ; width:100px;" src = "php.png">
+            </div>
+           
+        </div>
+
+
+    </body>
+        <footer>
+        </footer>
+    </body>
+
+
+
+
+
+    
+</body>
+</html>
